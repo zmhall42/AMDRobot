ff28c239778ed074f4900319dfb58ba1f75cc2ba
Zach Hall <zmhall42@tntech.edu>
2021-04-22 19:21:41 -0500

    Update PWM_motor_test1.py
    Added second motor control.

--- PWM_motor_test1.py
+++ PWM_motor_test1.py
@@ -3,11 +3,25 @@
 # 2021-04-22
 
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(32,GPIO.OUT)			#GPIO 12 PWM 0
 
-soft_pwm = GPIO.PWM(32,1000)	#set PWM on pin 32 to 1 kHz PWM clock
-soft_pwm.start(50)				#50% duty cycle
+m1_pwm_pin = 32	#GPIO 12 PWM for M1
+m1_dir_pin = 29	#GPIO 5 DIR for M1 
+m2_pwm_pin = 12 #GPIO 18 PWM for M2
+m2_dir_pin = 36	#GPIO 16 DIR for M2
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(m1_pwm_pin, GPIO.OUT)
+GPIO.setup(m1_dir_pin, GPIO.OUT)
+GPIO.setup(m2_pwm_pin, GPIO.OUT)
+GPIO.setup(m2_dir_pin, GPIO.OUT)
+
+GPIO.output(m1_dir_pin, GPIO.HIGH)	#set direction fwd = 1, rev = 0 
+GPIO.output(m2_dir_pin, GPIO.LOW)
+m1_pwm = GPIO.PWM(m1_pwm_pin, 1000)	#set PWM on pin 32 to 1 kHz PWM clock
+m2_pwm = GPIO.PWM(m2_pwm_pin, 1000)
+m1_pwm.start(25)			#25% duty cycle
+m2_pwm.start(25)
 input("Press return to stop: ")
-soft_pwm.stop()
+m1_pwm.stop()
+m2_pwm.stop()
 GPIO.cleanup()
